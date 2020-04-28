04d87a5b87be3357081adce4f6c11d80788d7f13
unknown <agroult@agylis.com>
2020-04-28 16:02:50 +0200

    Added PUT / endpoint

--- server.js
+++ server.js
@@ -42,6 +42,18 @@ app.post("/", (req, res) => {
     .catch((e) => res.sendStatus(404)); // In case of problem we send an HTTP code
 });
 
+app.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { last_name } = req.body;
+  pool // We're using the instance connected to the DB
+    .query("UPDATE users SET last_name=$1 WHERE id=$2 RETURNING *;", [
+      last_name,
+      id,
+    ])
+    .then((data) => res.json(data)) // We can send the data as a JSON
+    .catch((e) => res.sendStatus(404)); // In case of problem we send an HTTP code
+});
+
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
