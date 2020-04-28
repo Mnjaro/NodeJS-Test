fcfffa29c590ba6c6a7dde937efd24df837b5485
unknown <agroult@agylis.com>
2020-04-28 15:30:05 +0200

    Added the database to server connection

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
+const { Pool } = require("pg");
 const port = 3000;
+
+// Configurating The connection between my server and the database
+const pool = new Pool({
+  user: "giyijzov",
+  host: "dumbo.db.elephantsql.com",
+  database: "giyijzov",
+  password: "cAh_iqICwB8MtOfEiPRePDTGHSrwnx7N",
+  port: 5432,
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
